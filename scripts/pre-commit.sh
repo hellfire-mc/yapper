8d376c16eed5026c7a1e963d70798b96d79a4410
Kaylen Dart <actuallyori@gmail.com>
2022-11-29 23:46:15 +0000

    chore: tidy up pre-commit script

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,8 +3,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"/../../ || exit
 
-ls -al
-
 ./gradlew spotlessApply
 
 git add $(git diff --name-only --cached)
